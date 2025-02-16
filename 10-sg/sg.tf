8bf34b2e597adc7bdc54fd7f9e3ba138a8b76e18
naga leela <nleelachandrala@gmail.com>
2025-02-16 07:35:26 +0530

    teraform

--- 10-sg/sg.tf
+++ 10-sg/sg.tf
@@ -126,12 +126,12 @@ resource "aws_security_group_rule" "app_alb_vpn" {
   security_group_id = module.app_alb_sg.sg_id
 }
 
-
-resource "aws_security_group_rule" "mysql_bastion" {
+resource "aws_security_group_rule" "mysql_vpn" {
   type              = "ingress"
   from_port         = 3306
   to_port           = 3306
   protocol          = "tcp"
-  source_security_group_id = module.bastion_sg.sg_id
+  source_security_group_id = module.vpn_sg.sg_id
   security_group_id = module.mysql_sg.sg_id
 }
+
--- 30-vpn/vpn.tf
+++ 30-vpn/vpn.tf
@@ -10,11 +10,14 @@ resource "aws_instance" "openvpn" {
   vpc_security_group_ids = [data.aws_ssm_parameter.vpn_sg_id.value]
   instance_type          = "t2.micro"
   subnet_id   = local.public_subnet_id
-  #user_data = file("user-data.sh")
+  user_data = file("user-data.sh")
   tags = merge(
     var.common_tags,
     {
         Name = "${var.project_name}-${var.environment}-vpn"
     }
   )
+}
+output "vpn_ip" {
+  value       = aws_instance.openvpn.public_ip
 }
